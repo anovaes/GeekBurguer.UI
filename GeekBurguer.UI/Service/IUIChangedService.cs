e905284b2df2b3338f025efcfedbe1209cbd6f66
tiago_castro@outlook.com <taccastro87>
2018-03-19 22:29:25 -0300

    Classe serviços

---
+++ GeekBurguer.UI/Service/IUIChangedService.cs
@@ -0,0 +1,12 @@
+﻿using GeekBurguer.UI.Contract;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+
+namespace GeekBurguer.Products.Service
+{
+    public interface IUIChangedService
+    {
+        void SendMessagesAsync();
+        void AddToMessageList(IEnumerable<EntityEntry<Product>> changes);
+    }
+}
